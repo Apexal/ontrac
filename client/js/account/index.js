5397d08b081fe2821daca226fc90e0eb2803b241
Frank Matranga <thefrankmatranga@gmail.com>
2016-12-01 17:26:51 -0500

    Minor fixes

--- client/js/account/index.js
+++ client/js/account/index.js
@@ -5,7 +5,6 @@ Module('account-index',
     () => {
         $('.course-button').each((index, element) => {
             const courseName = $(element).data('course-name');
-            console.log(courseName);
             $(element).click(() => {
                 if (confirm(`Are you sure you want to remove ${courseName}?`)) {
                     $.post('/account/removecourse', { 'course-name': courseName }, (data) => {
